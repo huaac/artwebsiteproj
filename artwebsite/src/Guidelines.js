dc766fc7b92ff2859b169a5aa8f51c9bc7326b3a
Jenny Quan <toxic_dreamer@MacBook-Air-8.attlocal.net>
2024-09-27 22:13:02 -0700

    started on guidelines + rough outline of locations

---
+++ artwebsite/src/Guidelines.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Guidelines.css'; // This assumes you are using a separate CSS file for styles
+
+const Guidelines = () => {
+  return (
+    <div className="guidelines-container">
+      <h1 className="header">Covid-19: Safety at the Studios</h1>
+      <p className="intro">
+        We provide a safe environment for our students. We are implementing various cleaning and
+        sanitizing protocols as well as modifying how we teach our students.
+      </p>
+      <p className="intro">
+        If after reading the details you understand them and want to start classes, please reach out to us about
+        reserving a spot in class.
+      </p>
+
+      <h2>Things we will be doing to make the studios safer for our students:</h2>
+      <ul>
+        <li>Implementing cleaning and sanitizing protocols during and after classes.</li>
+        <li>Setting up art stations for each student before the start of class.</li>
+        <li>Having students clean up one at a time at the end of class.</li>
+      </ul>
+
+      <h2>Student guidelines for attending art classes:</h2>
+      <ul>
+        <li>Any students showing signs of sickness (coughing, running nose, etc…) will not be able to attend class.</li>
+        <li>Bring a smock/apron to class: until further notice we will not be providing use of our art smocks at the studios. A shirt or something that they can wear or put on their lap that will protect their clothes will do.</li>
+        <li>At this time face masks are optional.</li>
+        <li>Students will be required to wash their hands:
+          <ul>
+            <li>Upon entering the studio, before they head to their art station.</li>
+            <li>Before browsing through the art library, such as the art books and step drawing books.</li>
+            <li>At the end of class before they leave.</li>
+          </ul>
+        </li>
+        <li>Students are more than welcome to bring their water bottle to class.</li>
+        <li>Students must bring their pastels, watercolor kits, and oil kits to class. We won’t be allowing students to pay to borrow studio supplies anymore.</li>
+        <li>We will provide supplies for students doing their free introductory art class.</li>
+        <li>Student drop off:
+          <ul>
+            <li>Parents will wait outside of the studio – unless for an emergency.</li>
+            <li>Parents will need to drop off their children at the door and pick them up outside of the door after class is over.</li>
+            <li>Drop your kids off no earlier than 5 minutes before their class start time.</li>
+            <li>If you have any questions while you are outside of the studio, you can reach the program directors via the studio phones:
+              <ul>
+                <li>Bonita 619-508-1299</li>
+                <li>La Mesa 619-857-1270</li>
+                <li>Santee 619-855-2787</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>Student pick up:</li>
+      </ul>
+      <p className="footer">
+        We appreciate your patience and understanding while we implement the above policies. This will allow us to open our studios in a safe manner.
+      </p>
+    </div>
+  );
+};
+
+export default Guidelines;
--- artwebsite/src/Locations.js
+++ artwebsite/src/Locations.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Locations = () => {
-  return <div>Locations</div>;
+  let navigate = useNavigate();
+
+  const guidelines = () => {
+    navigate('/Guidelines');
+  };
+  return (
+    <div>
+      <div className='advertise'>
+        <h2 style={{ color: 'black', textAlign: 'center', paddingTop: '150px', fontSize: '30px' }}>
+              Call us today to schedule a free introductory art lesson!
+        </h2>
+        <h3 style={{ color: 'black', textAlign: 'left', paddingTop: '15px', paddingLeft: '22px', fontSize: '20px' }}>
+          (Only applicable to new students ages 5-17)
+        </h3>
+      </div>
+
+      <div className='guidelines'>
+        <h2 style={{ color: 'black', textAlign: 'center', paddingTop: '150px', fontSize: '30px' }}>
+             We strive to provide a safe environment for all students.
+             Please take a look at our studio safety guidelines.
+        </h2>
+        <button onClick={guidelines}>Safety Guidelines</button>
+      </div>
+
+      <div className='money'>
+        <div className='tuition'>
+          <h2 style={{ color: 'black', textAlign: 'center', paddingTop: '150px', fontSize: '30px' }}>
+                Need to Pay Tuition?
+          </h2>
+          <button>
+            Pay Tuition
+          </button>
+        </div>
+
+        <div className='supplies'>
+          <h2 style={{ color: 'black', textAlign: 'center', paddingTop: '150px', fontSize: '30px' }}>
+                Need more Art Supplies?
+          </h2>
+          <button>
+            Purchase Art Supplies
+          </button>
+        </div>
+      </div>
+
+      <div className='location-tabs'>
+        <button>
+          La Mesa
+        </button>
+        <button>
+          Bonita
+        </button>
+        <button>
+          Santee
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Locations;
