4066f84f696c77784ddb1d83e9c1ce546d412d32
Jenny Quan <toxic_dreamer@MacBook-Air-8.attlocal.net>
2024-11-27 18:30:19 -0800

    added in homepage sections

--- artwebsite/src/Homepage2.js
+++ artwebsite/src/Homepage2.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Homepage2.css';
 import LocationCards from './LocationCards';
+import Slides from './Slide'
+import Footer from './Footer'
 
 const Homepage2 = () => {
+  const OPTIONS = { loop: true }
+  const SLIDE_COUNT = 5
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
   return (
     <div className='homepage'>
       <div className='carousel'>  
-
+        <Slides slides={SLIDES} options={OPTIONS} />
       </div>
       <div className='mission'>
 
@@ -15,7 +20,7 @@ const Homepage2 = () => {
         <LocationCards />
       </div>
       <div className='footer'>
-
+        <Footer />
       </div>
     </div>
   );
